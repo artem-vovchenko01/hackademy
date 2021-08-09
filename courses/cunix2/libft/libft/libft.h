3f0bceb032116a41c4291d2f865d338d61af3249
Artem Vovchenko <a.vovchenko01@gmail.com>
2021-08-09 21:42:48 +0300

    Complete isascii

--- courses/cunix2/libft/libft/libft.h
+++ courses/cunix2/libft/libft/libft.h
@@ -8,5 +8,6 @@ char *ft_strchr(const char *str, int c);
 char *ft_strrchr(const char *str, int c);
 int ft_isalpha(int c);
 int ft_isdigit(int c);
+int ft_isascii(int c);
 
 size_t ft_strlen(const char *s);
