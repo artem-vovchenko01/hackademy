6b51d410bfb543cdb1b19b4aafa984a1f497c26c
Artem Vovchenko <a.vovchenko01@gmail.com>
2021-08-09 21:33:49 +0300

    Complete isdigit. Remove unnecessary include

--- courses/cunix2/libft/libft/libft.h
+++ courses/cunix2/libft/libft/libft.h
@@ -7,5 +7,6 @@ int ft_strncmp(const char *s1, const char *s2, size_t n);
 char *ft_strchr(const char *str, int c);
 char *ft_strrchr(const char *str, int c);
 int ft_isalpha(int c);
+int ft_isdigit(int c);
 
 size_t ft_strlen(const char *s);
