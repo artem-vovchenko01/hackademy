53de806e58fc17e186107d186fcb7080631aa184
Artem Vovchenko <a.vovchenko01@gmail.com>
2021-09-14 15:31:20 +0300

    Implement ProofOfWOrk

--- courses/golang/ex08-blockchain/blockchain.go
+++ courses/golang/ex08-blockchain/blockchain.go
@@ -1,30 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"bytes"
 	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 	"time"
 )
 
+const targetBits = 16
+const maxNonce = math.MaxInt64
+
 type Block struct {
-	timestamp int64
-	Data			[]byte
-	PrevBlockHash 	[]byte
-	Hash			[]byte
+	timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
 }
 
 func (b *Block) SetHash() {
 	timestamp := []byte(strconv.FormatInt(b.timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte {})
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
 	hash := sha256.Sum256(headers)
 	b.Hash = hash[:]
 }
 
-func NewBlock(data string, prevBlockHash []byte ) *Block {
-	block := &Block {time.Now().Unix(), []byte(data), prevBlockHash, []byte{} }
-	block.SetHash()
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
 	return block
 }
 
@@ -33,7 +43,7 @@ type Blockchain struct {
 }
 
 func (bc *Blockchain) AddBlock(data string) *Block {
-	prevBlock := bc.blocks[len(bc.blocks) - 1]
+	prevBlock := bc.blocks[len(bc.blocks)-1]
 	newBlock := NewBlock(data, prevBlock.Hash)
 	bc.blocks = append(bc.blocks, newBlock)
 	return newBlock
@@ -56,6 +66,76 @@ func main() {
 		fmt.Printf("PrevBlockHash: %x\n", v.PrevBlockHash)
 		fmt.Printf("Hash: %x\n", v.Hash)
 		fmt.Printf("Data: %s\n", v.Data)
+		pow := NewProofOfWork(v)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println("----------")
+	}
+}
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+	pow := &ProofOfWork{b, target}
+	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join([][]byte{
+		pow.block.PrevBlockHash,
+		pow.block.Data,
+		IntToHex(pow.block.timestamp),
+		IntToHex(int64(targetBits)),
+		IntToHex(int64(nonce)),
+	}, []byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Printf("\nProof nonce: %d", nonce)
+	fmt.Print("\n\n")
+	return nonce, hash[:]
+}
+
+func IntToHex(i int64) []byte {
+	return []byte(strconv.FormatInt(i, 16))
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	nonce := pow.block.Nonce
+	data := pow.prepareData(nonce)
+	hash := sha256.Sum256(data)
+	var hashInt big.Int
+	hashInt.SetBytes(hash[:])
+	var blockHash big.Int
+	blockHash.SetBytes(pow.block.Hash[:])
+	if hashInt.Cmp(&blockHash) == 0 && hashInt.Cmp(pow.target) == -1 {
+		return true
+	} else {
+		return false
 	}
 }
