6fe30f4d4791a291555a4f6872eb8eeae446e642
Artem Vovchenko <a.vovchenko01@gmail.com>
2021-09-17 17:14:55 +0300

    Trap SIGTERM in ex10

--- courses/golang/ex10-workerpool/goroutines.go
+++ courses/golang/ex10-workerpool/goroutines.go
@@ -7,17 +7,21 @@ import (
 	"strconv"
 	"fmt"
 	"sync"
+	"syscall"
+	"os/signal"
 )
 
 var wg sync.WaitGroup = sync.WaitGroup{}
 
 func Run(poolSize int) {
+
 	maxJobs := 1000
 	jobs := make(chan float64, maxJobs)
 	done := make(chan struct{}, 1)
+	go trapSigTerm(done)
 	input := make(chan string, 1)
 	curWorker := 1
-	go waitSeconds(10, done)
+	// go waitSeconds(10, done)
 	go readInput(input)
 	fromloop:
 	for {
@@ -25,15 +29,18 @@ func Run(poolSize int) {
 		case <- done:
 			break fromloop
 		case task := <- input:
+			if len(task) == 0 {
+				break
+			}
+			job, err := strconv.ParseFloat(task[:len(task) - 1], 64)
+			if err != nil {
+				break
+			}
 			if curWorker <= poolSize {
 				fmt.Printf("worker:%d spawning\n", curWorker)
 				wg.Add(1)
 				go worker(curWorker, jobs)
 				curWorker++
-			}
-			job, err := strconv.ParseFloat(task[:len(task) - 1], 64)
-			if err != nil {
-				panic(err)
 			}
 			jobs <- job
 		}
@@ -63,4 +70,11 @@ func worker(id int, jobs <- chan float64) {
 	}
 	fmt.Printf("worker:%d stopping\n", id)
 	wg.Done()
+}
+
+func trapSigTerm(done chan struct{}) {
+	c := make(chan os.Signal, 1) 
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+	done <- struct{}{}
 }
