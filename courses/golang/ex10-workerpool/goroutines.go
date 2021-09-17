4da97cc78998d2c1af3bf186896459fe5fd5b43a
Artem Vovchenko <a.vovchenko01@gmail.com>
2021-09-17 13:11:49 +0300

    Implement ex10

--- courses/golang/ex10-workerpool/goroutines.go
+++ courses/golang/ex10-workerpool/goroutines.go
@@ -1,4 +1,66 @@
 package goroutines
 
+import (
+	"os"
+	"bufio"
+	"time"
+	"strconv"
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup = sync.WaitGroup{}
+
 func Run(poolSize int) {
+	maxJobs := 1000
+	jobs := make(chan float64, maxJobs)
+	done := make(chan struct{}, 1)
+	input := make(chan string, 1)
+	curWorker := 1
+	go waitSeconds(10, done)
+	go readInput(input)
+	fromloop:
+	for {
+		select {
+		case <- done:
+			break fromloop
+		case task := <- input:
+			if curWorker <= poolSize {
+				fmt.Printf("worker:%d spawning\n", curWorker)
+				wg.Add(1)
+				go worker(curWorker, jobs)
+				curWorker++
+			}
+			job, err := strconv.ParseFloat(task[:len(task) - 1], 64)
+			if err != nil {
+				panic(err)
+			}
+			jobs <- job
+		}
+	}
+	close(jobs)
+	wg.Wait()
+}
+
+func readInput(inp chan string) {
+	for {
+		taskReader := bufio.NewReader(os.Stdin)
+		task, _ := taskReader.ReadString('\n')
+		inp <- task
+	}
+}
+
+func waitSeconds(sec int, done chan struct{}) {
+	time.Sleep(time.Duration(sec) * time.Second)
+	done <- struct{}{}
+}
+
+func worker(id int, jobs <- chan float64) {
+	for j := range jobs {
+		fmt.Printf("worker:%d sleep:%.1f\n", id, j)
+		time.Sleep(time.Duration(j) * time.Second)
+		// fmt.Printf("worker:%d slept %f\n", id, j)
+	}
+	fmt.Printf("worker:%d stopping\n", id)
+	wg.Done()
 }
