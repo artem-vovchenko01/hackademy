6159841394e945ca3ec751ebd9a4d09a98f07ab4
Artem Vovchenko <a.vovchenko01@gmail.com>
2021-09-18 20:19:44 +0300

    Implement orderbook - ex11

--- courses/golang/ex11-orderbook/orderbook.go
+++ courses/golang/ex11-orderbook/orderbook.go
@@ -1,15 +1,121 @@
 package orderbook
 
+import (
+	"container/heap"
+	"time"
+)
 type Orderbook struct {
-	// TODO
+	bids *PriorityQueue
+	asks *PriorityQueue
+	completed []*Order
 }
 
 func New() *Orderbook {
-	// TODO
-	return &Orderbook{}
+	var bid PriorityQueue = make(PriorityQueue, 0)
+	var ask PriorityQueue = make(PriorityQueue, 0)
+	return &Orderbook{ 
+		bids: &bid,
+		asks: &ask,
+	 }
 }
 
 func (orderbook *Orderbook) Match(order *Order) ([]*Trade, *Order) {
-	// TODO
-	return nil, nil
+	if order.Side == SideAsk {
+		return orderbook.MatchAsk(order)
+	} else {
+		return orderbook.MatchBid(order)
+	}
+}
+
+func (orderbook *Orderbook) MatchAsk(order *Order) ([]*Trade, *Order) {
+	trades := make([]*Trade, 0)
+	for {
+		if orderbook.bids.Len() == 0 {
+			if order.Kind == KindLimit {
+				item := &QueueItem {value: *order, timestamp: time.Now(), priority: (^uint64(0)) - order.Price}
+				heap.Push(orderbook.asks, item)
+			} else {
+				return trades, order
+			}
+			break
+		}
+		
+		highestBidQueueItem := heap.Pop(orderbook.bids)
+		highestBid := highestBidQueueItem.(*QueueItem).value
+		heap.Push(orderbook.bids, highestBidQueueItem)
+		if order.Kind == KindMarket || order.Price <= highestBid.Price {
+			trade := executeTrade(order, &highestBid)
+			trades = append(trades, trade)
+			if highestBid.Volume == 0 {
+				heap.Pop(orderbook.bids)
+			}
+			if order.Volume == 0 {
+				break
+			}
+		} else {
+			item := &QueueItem {value: *order, timestamp: time.Now(), priority: (^uint64(0)) - order.Price}
+			heap.Push(orderbook.asks, item)
+			break
+		}
+	}
+
+	return trades, nil
+}
+
+func (orderbook *Orderbook) MatchBid(order *Order) ([]*Trade, *Order) {
+	trades := make([]*Trade, 0)
+	for {
+		if orderbook.asks.Len() == 0 {
+			if order.Kind == KindLimit {
+				item := &QueueItem{value: *order, timestamp: time.Now(), priority: order.Price}
+				heap.Push(orderbook.bids, item)
+			} else {
+				return trades, order
+			}
+			break
+		}
+
+		lowestAskQueueItem := heap.Pop(orderbook.asks)
+		lowestAsk := lowestAskQueueItem.(*QueueItem).value
+		heap.Push(orderbook.asks, lowestAskQueueItem)
+		if order.Kind == KindMarket || order.Price >= lowestAsk.Price {
+			trade := executeTrade(order, &lowestAsk)
+			trades = append(trades, trade)
+			if lowestAsk.Volume == 0 {
+				heap.Pop(orderbook.asks)
+			}
+			if order.Volume == 0 {
+				break
+			}
+		} else {
+			item := &QueueItem{value: *order, timestamp: time.Now(), priority: order.Price}
+			heap.Push(orderbook.bids, item)
+			break
+		}
+	}
+
+	return trades, nil
+}
+
+func executeTrade(incoming *Order, resting *Order) *Trade {
+	var amount uint64
+	if resting.Volume >= incoming.Volume {
+		amount = incoming.Volume
+	} else {
+		amount = resting.Volume
+	}
+
+	incoming.Volume -= amount
+	resting.Volume -= amount
+	if incoming.Side == SideAsk {
+		return &Trade {Volume: amount, Price: resting.Price, Ask: incoming, Bid: resting }
+	} else {
+		return &Trade {Volume: amount, Price: resting.Price, Ask: resting, Bid: incoming }
+	}
+}
+
+func (pq *PriorityQueue) update(item *QueueItem, value Order) {
+	item.value = value
+	item.priority = item.value.Price
+	heap.Fix(pq, item.index)
 }
---
+++ courses/golang/ex11-orderbook/prioirity_queue.go
@@ -0,0 +1,78 @@
+package orderbook
+
+import (
+	"strconv"
+	"time"
+)
+
+type QueueItem struct {
+	value    Order
+	priority uint64
+	timestamp time.Time
+	index int
+}
+
+func (qi *QueueItem) String() string {
+	return "Side: " + qi.value.Side.String() + ", kind: " + qi.value.Kind.String() + ", Volume: " + strconv.FormatUint(qi.value.Volume, 10) + ", Price: " + strconv.FormatUint(qi.value.Price, 10)
+}
+
+type PriorityQueue []*QueueItem
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	if pq[i].priority == pq[j].priority {
+		return pq[i].timestamp.Before(pq[j].timestamp)
+	}
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = int(i)
+	pq[j].index = int(j)
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*QueueItem)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	item.index = -1
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) CanFillAmount(requestedAmount uint64) bool {
+	old := *pq
+	n := len(old)
+	if n == 0 {
+		return false
+	}
+	amount := uint64(0) 
+	for amount < requestedAmount {
+		if n == 0 {
+			return false
+		}
+		n--
+		amount += old[n].value.Volume
+	}
+	return true
+}
+
+func (pq *PriorityQueue) Peek() (interface{}, bool) {
+	old := *pq
+	n := len(old)
+	if n == 0 {
+		return nil, false
+	}
+	item := old[n-1]
+	return item, true
+}
